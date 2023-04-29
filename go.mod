806e4c74bd3cd71038a621fe2e3df30fe84c1b04
Cristian Flores <carmando.fa05@gmail.com>
2023-04-28 21:06:55 -0600

    Final commit

---
+++ go.mod
@@ -0,0 +1,13 @@
+module github.com/CristFlores/myls
+
+go 1.20
+
+require golang.org/x/exp v0.0.0-20230425010034-47ecfdc1ba53
+
+require (
+	github.com/AJRDRGZ/fileinfo v0.0.0-20230215213109-b9a695b817d1 // indirect
+	github.com/fatih/color v1.15.0 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.17 // indirect
+	golang.org/x/sys v0.6.0 // indirect
+)
--- main.go
+++ main.go
@@ -7,12 +7,15 @@ import (
 	"fmt"
 	"io/fs"
 	"os"
+	"path"
 	"regexp"
 	"runtime"
 	"sort"
 	"strings"
 	"time"
 
+	"github.com/AJRDRGZ/fileinfo"
+	"github.com/fatih/color"
 	"golang.org/x/exp/constraints"
 )
 
@@ -147,7 +150,7 @@ func orderBySize(files []file, isReverse bool) {
 func printList(fs []file, nRecords int) {
 	for _, file := range fs[:nRecords] {
 		style := mapStyleByFileType[file.fileType]
-		fmt.Printf("%s %s %s %10d %s %s %s %s\n", file.mode, file.userName, file.groupName, file.size, file.modificationTime.Format(time.DateTime), style.icon, file.name, style.symbol)
+		fmt.Printf("%s %s %s %10d %s %s %s %s %s\n", file.mode, file.userName, file.groupName, file.size, file.modificationTime.Format(time.DateTime), style.icon, setcolor(file.name, style.color), style.symbol, markHidden(file.isHidden))
 	}
 }
 
@@ -157,13 +160,15 @@ func getFile(dir fs.DirEntry, isHidden bool) (file, error) {
 		return file{}, fmt.Errorf("dir.Info(): %v", err)
 	}
 
+	userName, groupName := fileinfo.GetUserAndGroup(info.Sys())
+
 	f:= file {
 		name: dir.Name(),
 		fileType: 0,
 		isDir: dir.IsDir(),
 		isHidden: isHidden,
-		userName: "cristian",
-		groupName: "ATS",
+		userName: userName,
+		groupName: groupName,
 		size: info.Size(),
 		modificationTime: info.ModTime(),
 		mode: info.Mode().String(),
@@ -189,6 +194,23 @@ func setFile (f *file) file {
 		f.fileType = fileRegular
 	}
 	return *f
+}
+
+func setcolor (nameFile string, styleColor color.Attribute) string {
+	switch styleColor {
+	case color.FgBlue:
+		return blue(nameFile)
+	case color.FgGreen:
+		return green(nameFile)
+	case color.FgRed:
+		return red(nameFile)
+	case color.FgMagenta:
+		return magenta(nameFile)
+	case color.FgCyan:
+		return cyan(nameFile)
+	}
+
+	return nameFile
 }
 
 func isLink(f file) bool {
@@ -217,5 +239,19 @@ func isImage(f file) bool {
 }
 
 func isHidden(fileName, basePath string) bool {
-	return strings.HasPrefix(fileName, ".")
+	// return strings.HasPrefix(fileName, ".")
+	filePath := fileName
+
+	if runtime.GOOS == Windows {
+		filePath = path.Join(basePath, fileName)
+	}
+
+	return fileinfo.IsHidden(filePath)
+}
+
+func markHidden(isHidden bool) string {
+	if !isHidden {
+		return ""
+	}
+	return yellow("ø")
 }
